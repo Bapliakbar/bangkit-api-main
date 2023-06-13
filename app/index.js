a5fb5605e8d8c23219be2280cc42b8175b9b3391
Rafli Akbar <bapliakbar@gmail.com>
2023-06-13 22:47:29 +0700

    refactor code

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import connectDB from "./database/mongodb.js";
 
 const app = express();
 
@@ -10,30 +11,35 @@ import articleRouter from "./routes/articleRoute.js";
 import pageNotFound from "./utils/pageNotFound.js";
 
 // CONFIGURE DATABASE
-import connectDB from "./database/mongodb.js";
+connectDB()
+  .then(() => {
+    // PORT AND PATH
+    const PORT = process.env.PORT || 8080;
+    const VERSION_API = "/api/v1";
+    const appendUrl = (url) => `${VERSION_API}${url}`;
 
-// PORT AND PATH
-const PORT = process.env.PORT || 8080;
-const VERSION_API = "/api/v1";
-const appendUrl = (url) => `${VERSION_API}${url}`;
+    // MIDDLEWARE
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-// MIDDLEWARE
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+    // ROUTER
+    app.use(appendUrl("/auth"), authRouter);
+    app.use(appendUrl("/article"), articleRouter);
 
-// ROUTER
-app.use(appendUrl("/auth"), authRouter);
-app.use(appendUrl("/article"), articleRouter)
+    // ENDPOINT NOT CREATED
+    app.use("/", pageNotFound);
 
-// ENDPOINT NOT CREATED
-app.use("/", pageNotFound);
-
-connectDB().then(() => {
-  app.listen(PORT, function () {
-    console.log(
-      "Express server listening on port %d in %s mode",
-      this.address().port,
-      app.settings.env,
-    );
+    return PORT;
+  })
+  .then((port) => {
+    app.listen(port, function () {
+      console.log(
+        "Express server listening on port %d in %s mode",
+        this.address().port,
+        app.settings.env
+      );
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
   });
-});
